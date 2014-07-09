fb7e6f3bf216d546a60ef8d0d2112946e6c71190
Samandriel <settawut.dk@gmail.com>
2014-07-09 18:14:41 +0700

    Customize Sidebar interface

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,5 +2,14 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  before_action :sidebar_values
+
+  def sidebar_values
+  	@food_types = FoodType.all
+  	@food_preferences = FoodPreference.all
+  	@cuisines = Cuisine.all 
+  end
+
+
 
 end
